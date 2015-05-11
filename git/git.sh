f238691f070421c996a42c82c4d03305a991e535
Mateusz Zając <zajac.mateusz@icloud.com>
2015-05-11 11:41:06 +0200

    Add prompt to git configuration

--- git/git.sh
+++ git/git.sh
@@ -2,11 +2,13 @@
 
 GIT_DIRECTORY=$HOME/.dotfiles/git
 SSH_DIRECTORY=$HOME/.ssh
-USER="Mateusz Zając"
-EMAIL="zajac.mateusz@icloud.com"
 
 # Config global git user
+echo -e "Please, enter your git username: "
+read USER
 git config --global user.name $USER
+echo -e "Please, enter your git user email address: "
+read EMAIL
 git config --global user.email $EMAIL
 
 # Credentials helper
